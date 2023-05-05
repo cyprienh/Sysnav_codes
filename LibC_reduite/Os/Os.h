2ef234085abe3f6197628eaf3e6cfe563774456b
Cyprien Heusse <c.heusse@gmail.com>
2023-05-05 14:41:59 +0200

    First commit

---
+++ LibC_reduite/Os/Os.h
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///  @file		Os.h
+///
+///  @brief		Inclus les librairies nécessaires au fonctionnement du système
+///
+///  @date		15/04/2014
+///  @author	N/A
+///  @copyright	SYSNAV
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef _OS_H
+#define _OS_H
+ 
+
+//définition du projet, nécessaire pour définir le type d'architecture
+#include <Os/Common/Utils/GenericTypeDefs.h>
+#include <Peripheral/Peripheral.h>
+#include <Time/Time.h>
+		
+		
+#endif //_OS_H
